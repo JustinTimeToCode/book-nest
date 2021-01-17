fb6918c617cc829f80da79cd86e8135f67c5a222
Justin Fagan <jbfagan7@gmail.com>
2021-01-17 01:09:23 -0500

    Completed Book services

--- src/books/books.dto.ts
+++ src/books/books.dto.ts
@@ -1,6 +1,21 @@
 import { IsString, IsInt, IsMongoId } from 'class-validator'
 import { Types } from 'mongoose'
 
+export class UpdateBookDTO {
+    @IsMongoId()
+    _id: Types.ObjectId;
+
+    @IsString()
+    title: string;
+
+    @IsInt()
+    copies: number;
+
+    @IsString()
+    genre: string;
+
+}
+
 export class CreateBookDTO {
     @IsString()
     title: string;
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -2,12 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { Book, BookDocument } from './books.model';
+import { CreateBookDTO, UpdateBookDTO } from './books.dto';
 
 @Injectable()
 export class BooksService {
   constructor(@InjectModel(Book.name) private bookModel: Model<BookDocument>) {}
 
-  async create(){
-      
+  async create(payload: CreateBookDTO): Promise<BookDocument> {
+    const book = new this.bookModel(payload);
+    return await book.save();
+  }
+
+  async getBookById(_id: Types.ObjectId): Promise<BookDocument> {
+    return await this.bookModel.findById(_id).exec();
+  }
+
+  async getBooks(): Promise<BookDocument[]> {
+    return await this.bookModel.find({}).exec();
+  }
+
+  async updateBook(payload: UpdateBookDTO): Promise<BookDocument> {
+    return await this.bookModel
+      .findByIdAndUpdate(payload._id, payload, { new: true })
+      .exec();
+  }
+
+  async deleteBook(_id: Types.ObjectId) {
+    return await this.bookModel.findByIdAndDelete(_id).exec();
   }
 }
